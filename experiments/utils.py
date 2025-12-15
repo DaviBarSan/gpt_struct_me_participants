631964da26d5061d9aa308319e0c3234702a65b6
Davi Santos <davibarrel98@gmail.com>
2025-12-15 18:44:35 -0300

    add phi and mistral in models

--- experiments/utils.py
+++ experiments/utils.py
@@ -1,57 +1,17 @@
 from src.models import (
-    chatgpt,
-    gpt3,
-    gpt4,
-    Falcon,
-    Llama2InferenceAPI,
-    HFInferenceLocal,
-    llama2_70b,
     gemini,
     qwen3_4b,
+    mistral_7b,
+    phi_4_6b
 )
 
 
 def mid2model(mid: str):
-    if mid == "gpt3":
-        return gpt3
-
-    elif mid == "chatgpt":
-        return chatgpt
-
-    elif mid == "gpt4":
-        return gpt4
-
-    elif mid == "falcon-7b":
-        return Falcon("7b", instruct=False)
-
-    elif mid == "falcon-7b-instruct":
-        return Falcon("7b", instruct=True)
-
-    elif mid == "falcon-40b":
-        return HFInferenceLocal()
-
-    elif mid == "falcon-40b-instruct":
-        return Falcon("40b", instruct=True)
-
-    elif mid == "llama2-7b":
-        return Llama2InferenceAPI("7b", chat=False)
-
-    elif mid == "llama2-13b":
-        return Llama2InferenceAPI("13b", chat=False)
-
-    elif mid == "llama2-70b":
-        return llama2_70b
-
-    elif mid == "llama2-7b-chat":
-        return Llama2InferenceAPI("7b", chat=True)
-
-    elif mid == "llama2-13b-chat":
-        return Llama2InferenceAPI("13b", chat=True)
-
-    elif mid == "llama2-70b-chat":
-        return Llama2InferenceAPI("70b", chat=True)
-    
-    elif mid == "gemini":
+    if mid == "gemini":
         return gemini
     elif mid == "qwen3_4b":
         return qwen3_4b
+    elif mid == "mistral_7b":
+        return mistral_7b
+    elif mid == "phi_4_6b":
+        return phi_4_6b
--- src/models.py
+++ src/models.py
@@ -14,7 +14,16 @@ import dotenv
 from transformers import AutoTokenizer, AutoModelForCausalLM
 # import torch
 # from text_generation import Client
-# from transformers import pipeline
+from transformers import pipeline
+
+##MISTRAL
+from mistral_inference.transformer import Transformer
+from mistral_inference.generate import generate
+
+from mistral_common.tokens.tokenizers.mistral import MistralTokenizer
+from mistral_common.protocol.instruct.messages import UserMessage
+from mistral_common.protocol.instruct.request import ChatCompletionRequest
+
 
 from src.utils import is_json
 
@@ -29,204 +38,6 @@ dotenv.load_dotenv(ROOT / ".env")
 HF_KEY = os.getenv("HF_KEY")
 # os.environ["REPLICATE_API_TOKEN"] = os.getenv("REPLICATE_KEY")
 
-
-class Falcon:
-    def __init__(self, size: str = "7b", instruct: bool = False):
-        """Initialize the Falcon model.
-        Args:
-            size (str): Size of the model to use. Valid values: "7b", "40b", and "180b". 
-        """
-        torch.cuda.empty_cache()
-
-        model_name = f"falcon-{size}"
-        if instruct:
-            model_name += "-instruct"
-        model_path = MODELS_PATH / model_name
-
-        logger.info(f"Loading Falcon model from {model_path}.")
-        self.tokenizer = AutoTokenizer.from_pretrained(model_path)
-        self.pipeline = transformers.pipeline(
-            "text-generation",
-            model=model_path,
-            tokenizer=self.tokenizer,
-            torch_dtype=torch.bfloat16,
-            trust_remote_code=True,
-            device_map="auto",
-        )
-
-    def __call__(self, prompt: str) -> str:
-        logger.info(f"Generating answer for prompt:\n{prompt}")
-        input_ids = self.tokenizer.encode(prompt, return_tensors="pt")
-        input_n_tokens = input_ids.shape[1]
-        output = self.pipeline(
-            prompt,
-            max_length=2 * input_n_tokens,
-            do_sample=False,  # Ensure deterministic results.
-            num_return_sequences=1,
-            eos_token_id=self.tokenizer.eos_token_id,  # For open-ended generation.
-            pad_token_id=self.tokenizer.eos_token_id,
-            use_cache=False,
-        )
-        answer = output[0]["generated_text"]
-        answer = answer.strip(prompt)
-        return answer
-
-
-class Llama2InferenceAPI:
-    def __init__(self, size: str = "70b", chat: bool = False):
-        """Initialize the Llama model.
-        Args:
-            size (str): Size of the model to use. Valid values: "70b" and "370b". 
-        """
-        if chat:
-            model_name = f"Llama-2-{size}-chat-hf"
-        else:
-            model_name = f"Llama-2-{size}-hf"
-
-        self.endpoint = f"https://api-inference.huggingface.co/models/meta-llama/{model_name}"
-        self.headers = {"Authorization": f"Bearer {HF_KEY}"}
-        
-    def __call__(self, prompt: str) -> str:
-        payload = {
-            "inputs": prompt,
-            "parameters": {
-                "max_new_tokens": 1_000,
-                "do_sample": False,
-                "return_full_text": False,
-            },
-            "options": {
-                "wait_for_model": True,
-                "use_cache": False,
-            }
-        }
-
-        response = requests.post(self.endpoint, headers=self.headers, json=payload)
-        output = response.json()
-        answer = output[0]["generated_text"]
-        return answer
-
-
-def gpt3(prompt: str, max_tokens: int = 400) -> str:
-    """Generate text with GPT-3."""
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=prompt,
-        temperature=0,
-        max_tokens=max_tokens,
-    )
-
-    content = response.to_dict()
-    answer = content["choices"][0]["text"]
-
-    return answer
-
-
-def chatgpt(prompt: str, max_tokens: int = 400) -> str:
-    """Generate text with ChatGPT."""
-
-    message = {"role": "system", "content": f"{prompt}"}
-
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[message],
-        temperature=0,
-        max_tokens=max_tokens,
-    )
-
-    content = response.to_dict()
-    answer = content["choices"][0]["message"]["content"]
-
-    return answer
-
-
-def gpt4(prompt: str, max_tokens: int = 400) -> str:
-    """Generate text with GPT-4."""
-
-    message = {"role": "system", "content": f"{prompt}"}
-
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=[message],
-        temperature=0.0,
-        max_tokens=max_tokens,
-    )
-
-    content = response.to_dict()
-    answer = content["choices"][0]["message"]["content"]
-
-    return answer
-
-
-class SagemakerEndpoint:
-    def __init__(self, endpoint: str):
-        self.endpoint = endpoint
-        self.runtime = boto3.Session().client('sagemaker-runtime')
-
-    def __call__(self, prompt: str) -> str:
-        payload = json.dumps({
-            "inputs": prompt,
-            "parameters": {
-                "eos_token_id": 2,
-                "pad_token_id": 2,
-                "max_length": 4096,
-                "temperature": 0,
-                "do_sample": False,
-                "num_return_sequences": 1,
-            },
-        })
-
-        response = self.runtime.invoke_endpoint(
-            EndpointName=self.endpoint,
-            ContentType="application/json",
-            Body=payload
-        )
-        answer = response["Body"].read().decode("utf-8")
-        return answer
-
-
-class HFInferenceLocal:
-    """A model deployed locally in 
-    [Text Generation Inference](https://github.com/huggingface/text-generation-inference) 
-    container from  Hugging Face."""
-
-    def __init__(self, url: str = "http://127.0.0.1:8080"):
-        self.client = Client(url, timeout=300)
-
-    def __call__(self, prompt: str, max_tokens: int = 1_000) -> str:
-        reponse = self.client.generate(
-            prompt=prompt,
-            max_new_tokens=max_tokens,
-            do_sample=False
-        )
-        answer = reponse.generated_text
-        return answer
-
-
-def llama2_70b(prompt: str, max_tokens: int = 800) -> str:
-    """API from [Replicate](https://replicate.com/). To minimize the number of tokens generated, 
-    this function checks if the tokens generated are valid JSON. If not, it continues generating 
-    until the max_tokens in reached."""
-    
-    payload = {
-        "prompt": prompt,
-        "max_new_tokens": max_tokens,
-        "temperature": 0.01,
-        "seed": 123,
-        "top_k": 1
-    }
-    
-    generator = replicate.run(
-        "meta/llama-2-70b:a52e56fee2269a78c9279800ec88898cecb6c8f1df22a6483132bea266648f00",
-        input=payload
-    )
-
-    tokens = []
-    for item in generator:
-        tokens.append(item)
-        answer = "".join(tokens)
-        if is_json(answer):
-            return answer
-    return answer
 
 def qwen3_4b(prompt: str):
     from transformers import pipeline
@@ -293,3 +104,47 @@ def gemini(prompt: str) -> str:
 
 # Example Usage (assuming you have your API key set as an environment variable)
 # print(gemini_generate_text("Explain the concept of quantum entanglement in simple terms."))
+
+def mistral_7b(prompt: str) -> str:    
+
+
+    relative_path = "C:\\Users\davib\Desktop\\MSc_DataScience\\thesis\gpt_struct_me\\resources\\models\\Mistral-7B"
+
+    tokenizer = MistralTokenizer.from_file(f"{relative_path}\\tokenizer.model.v3")
+    model = Transformer.from_folder(relative_path)
+    completion_request = ChatCompletionRequest(messages=[UserMessage(content=prompt)])
+    tokens = tokenizer.encode_chat_completion(completion_request).tokens
+
+    out_tokens, _ = generate([tokens], model, max_tokens=16384, temperature=0.0, eos_id=tokenizer.instruct_tokenizer.tokenizer.eos_id)
+    result = tokenizer.instruct_tokenizer.tokenizer.decode(out_tokens[0])
+
+    print(result)
+    return result
+
+def phi_4_6b(prompt: str):
+    # 1. Define the relative path you used
+    relative_path = "C:\\Users\davib\Desktop\\MSc_DataScience\\thesis\gpt_struct_me\\resources\models\Phi4-6B"
+
+    # # 2. Convert the relative path to an absolute path
+    # os.path.abspath() handles relative parts like '..'
+    absolute_model_path = os.path.abspath(relative_path)
+    print(f"Loading model from absolute path: {relative_path}")
+
+    # 3. Pass the ABSOLUTE path to the pipeline
+    pipe = pipeline("text-generation", model=relative_path)
+    messages = [
+        {"role": "user", "content": prompt},
+    ]
+    # 2. Call the pipeline and store the result
+# Crucial Argument: return_full_text=False
+    output = pipe(
+        messages,
+        max_new_tokens=16384,
+        return_full_text=False      # Tells the pipeline to return ONLY the generated text
+    )
+
+    # 3. Extract the final text
+    # The output is a list of dictionaries, so we need to drill down.
+    generated_text = output[0]['generated_text']
+    print(generated_text)
+    return generated_text
