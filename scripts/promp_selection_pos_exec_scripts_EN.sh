70447f78504f08c41057793e96e2fc9f23affaed
Davi Santos <davibarrel98@gmail.com>
2026-07-31 17:14:10 +0100

    resolve conflicts

--- scripts/promp_selection_pos_exec_scripts_EN.sh
+++
@@ -1,2 +0,0 @@
-python -m experiments.parse prompt_selection --language english
-python -m experiments.evaluate prompt_selection --language english
--- scripts/test_pos_exec_scripts.sh
+++ scripts/test_pos_exec_scripts.sh
@@ -1,2 +1,2 @@
-python -m experiments.parse test
+python -m experiments.parse test portuguese --prompt_name_variations "Yes"
 python -m experiments.evaluate test
