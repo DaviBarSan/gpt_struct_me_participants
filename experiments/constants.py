fe6ca5b53a1baf94379d97c2ae2b241cc285723f
Davi Barrel Santos <davibarrel98@gmail.com>
2026-08-03 22:30:03 +0100

    add euro-llm-9b results

--- experiments/constants.py
+++ experiments/constants.py
@@ -82,6 +82,7 @@ BEST_TEMPLATES = {
         ("qwen3_8b", "participants"): "ext_def_exp",
         ("gemma3_12b", "participants"): "cls_def_exp",
         ("gemma3_4b", "participants"): "cls_def_exp",
+        ("euro_llm_9b", "participants"): "cls_def_exp",
     },
 
     "english": {
@@ -94,6 +95,7 @@ BEST_TEMPLATES = {
         ("qwen3_8b", "participants"): "ext_exp",
         ("gemma3_12b", "participants"): "cls_exp",
         ("gemma3_4b", "participants"): "cls_def_exp",
+        ("euro_llm_9b", "participants"): "cls_exp",
     }
 }
 
